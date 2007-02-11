21e962a3427d6e208e0f586fb8191e1e835899b0
Georg Lundesgaard <georglundesgaard@users.noreply.github.com>
2007-02-11 20:59:45 +0000

    Added trie set test.

---
+++ src/test/java/dk/lundesgaard/util/TrieSetTest.java
@@ -0,0 +1,30 @@
+package dk.lundesgaard.util;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class TrieSetTest extends TestCase {
+
+    /**
+     * Create the test case.
+     *
+     * @param testName name of the test case
+     */
+    public TrieSetTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( TrieSetTest.class );
+    }
+    
+    public void testSet() {
+    	// TODO
+    }
+}
